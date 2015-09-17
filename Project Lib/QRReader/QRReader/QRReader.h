931841fcb86450e778b27c36d534e1e6df20fda2
Roger Molas <roger@klab.com>
2015-09-17 11:34:06 +0800

    Set up Static Lib project

---
+++ Project Lib/QRReader/QRReader/QRReader.h
@@ -0,0 +1,13 @@
+//
+//  QRReader.h
+//  QRReader
+//
+//  Created by Roger Molas on 9/17/15.
+//  Copyright © 2015 Roger Molas. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface QRReader : NSObject
+
+@end
---
+++ Project Lib/QRReader/QRReader/QRReader.m
@@ -0,0 +1,13 @@
+//
+//  QRReader.m
+//  QRReader
+//
+//  Created by Roger Molas on 9/17/15.
+//  Copyright © 2015 Roger Molas. All rights reserved.
+//
+
+#import "QRReader.h"
+
+@implementation QRReader
+
+@end
